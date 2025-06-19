0f41310598ff2b71e99c4a26de04213a292c57cb
Arvin Buid <arvinbuid01@gmail.com>
2025-06-19 18:55:07 +0800

    Create small chat app using socket.io.

---
+++ chat.js
@@ -0,0 +1,21 @@
+import express from "express";
+import path from "path";
+import {fileURLToPath} from "url";
+import {Server} from "socket.io";
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(__dirname + "/public"));
+
+const expressServer = app.listen(8001);
+const io = new Server(expressServer);
+
+io.on("connection", (socket) => {
+  console.log(socket.id + " connected");
+  socket.on("newMessageFromClient", (dataFromClient) => {
+    io.emit("newMessageToClients", {message: dataFromClient.message}); // Sent to all sockets
+  });
+});
