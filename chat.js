307a59ee64c639dcd6e86c8a4890953e197a6ae0
Arvin Buid <arvinbuid01@gmail.com>
2025-06-20 10:54:33 +0800

    Add some info comments about Server options from the docs.

--- chat.js
+++ chat.js
@@ -11,7 +11,20 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(__dirname + "/public"));
 
 const expressServer = app.listen(8001);
-const io = new Server(expressServer);
+
+// io - the server object in socket.io docs
+const io = new Server(expressServer, {
+  cors: {
+    origin: "http://localhost:8001",
+  },
+  transports: ["polling", "websocket"],
+  cookie: {
+    name: "my-cookie",
+    httpOnly: true,
+    sameSite: "strict",
+    maxAge: 86400, // 1 day
+  },
+});
 
 io.on("connection", (socket) => {
   console.log(socket.id + " connected");
