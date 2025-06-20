f1a1723f8491d53b78c9c96c7d798813ccfea9e5
Arvin Buid <arvinbuid01@gmail.com>
2025-06-20 18:25:07 +0800

    Setup slack clone project and refresher.

---
+++ slackClone/public/scripts.js
@@ -0,0 +1,11 @@
+// client
+const socket = io("http://localhost:9000");
+
+socket.on("connect", () => {
+  console.log("Connected!");
+  socket.emit("clientConnect");
+});
+
+socket.on("welcome", (data) => {
+  console.log(data);
+});
---
+++ slackClone/slack.js
@@ -0,0 +1,16 @@
+// server
+const express = require("express");
+const app = express();
+const socketio = require("socket.io");
+
+app.use(express.static(__dirname + "/public"));
+
+const expressServer = app.listen(9000);
+const io = socketio(expressServer);
+
+io.on("connection", (socket) => {
+  socket.emit("welcome", "Welcome to the server!");
+  socket.on("clientConnect", () => {
+    console.log(socket.id + " is connected");
+  });
+});
