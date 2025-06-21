417012783e2b2c7ccc5ded6282ee36540378dec0
Arvin Buid <arvinbuid01@gmail.com>
2025-06-21 18:05:08 +0800

    Add rooms to DOM.

--- slackClone/public/scripts.js
+++ slackClone/public/scripts.js
@@ -21,7 +21,21 @@ socket.on("welcome", (data) => {
 socket.on("nsList", (nsData) => {
   const nsDiv = document.querySelector(".namespaces");
   nsData.forEach((ns) => {
-    nsDiv.innerHTML += `<div class="namespace" ns=${ns.name}><img src=${ns.image}></div>`;
+    nsDiv.innerHTML += `<div class="namespace" ns=${ns.endpoint}><img src=${ns.image}></div>`;
   });
-  console.log(nsData);
+
+  Array.from(document.getElementsByClassName("namespace")).forEach((ns) => {
+    ns.addEventListener("click", () => {
+      const nsEndpoint = ns.getAttribute("ns"); // get the ns attribute
+      const clickedNs = nsData.find((ns) => ns.endpoint === nsEndpoint);
+      const rooms = clickedNs.rooms;
+
+      const roomsList = document.querySelector(".room-list");
+      roomsList.innerHTML = "";
+      // loop through each room and add it to the dom
+      rooms.forEach((room) => {
+        roomsList.innerHTML += `<li><span class="glyphicon glyphicon-lock"></span>${room.roomTitle}</li>`;
+      });
+    });
+  });
 });
