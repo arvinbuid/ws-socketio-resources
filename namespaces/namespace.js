d3b68afd8b3e570a6862467779ffa3cdda27c7ac
Arvin Buid <arvinbuid01@gmail.com>
2025-06-21 22:35:43 +0800

    Understanding Rooms from docs.

--- namespaces/namespace.js
+++ namespaces/namespace.js
@@ -10,12 +10,19 @@ const io = socketio(expressServer);
 io.of("/").on("connection", (socket) => {
   // io.on("connection", (socket) => {
   console.log(socket.id + " connected");
+
+  socket.join("room1");
+  socket.join("room2");
+  io.of("/").to("room1").emit("welcomeToRoom1", {});
+  io.of("/").to("room1").to("room2").emit("welcomeToRoom1", {});
   socket.on("newMessageFromClient", (dataFromClient) => {
     io.emit("newMessageToClients", {message: dataFromClient.message}); // Sent to all sockets
   });
 });
 
+// rooms are namespace specific
 io.of("/admin").on("connection", (socket) => {
-  // io.on("connection", (socket) => {
   console.log(socket.id + " connected to admin");
+  socket.join("room1");
+  io.of("/admin").to("room1").emit("welcomeToRoom1", {});
 });
