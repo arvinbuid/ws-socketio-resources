954b3888bced81e662fc43774fe243ae2312896a
Arvin Buid <arvinbuid01@gmail.com>
2025-06-21 21:45:54 +0800

    Understanding namespaces from docs, add comments in namespace files, & update the code.

--- namespaces/namespace.js
+++ namespaces/namespace.js
@@ -1,21 +1,21 @@
-import express from "express";
-import path from "path";
-import {fileURLToPath} from "url";
-import {Server} from "socket.io";
-
+const express = require("express");
 const app = express();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const socketio = require("socket.io");
 
 app.use(express.static(__dirname + "/public")); // Serve static files
 
 const expressServer = app.listen(8001);
-const io = new Server(expressServer);
+const io = socketio(expressServer);
 
-io.on("connection", (socket) => {
+io.of("/").on("connection", (socket) => {
+  // io.on("connection", (socket) => {
   console.log(socket.id + " connected");
   socket.on("newMessageFromClient", (dataFromClient) => {
     io.emit("newMessageToClients", {message: dataFromClient.message}); // Sent to all sockets
   });
+});
+
+io.of("/admin").on("connection", (socket) => {
+  // io.on("connection", (socket) => {
+  console.log(socket.id + " connected to admin");
 });
