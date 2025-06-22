e49a18b0a1a9997e58ad4eea2a5200f35c83d283
Arvin Buid <arvinbuid01@gmail.com>
2025-06-22 15:42:45 +0800

    Implementing nsChange continued...

--- slackClone/public/scripts.js
+++ slackClone/public/scripts.js
@@ -5,32 +5,54 @@
 // Temp remove prompt for development
 const userName = "John";
 const userPassword = "x";
-
 const socket = io("http://localhost:9000");
 
+// sockets will be put into this array, in the index of their ns.id
+const nameSpaceSockets = [];
+const listeners = {
+  nsChange: [],
+};
+
+const addListeners = (nsId) => {
+  // nameSpaceSockets[ns.id] = thisNs;
+  if (!listeners.nsChange[nsId]) {
+    nameSpaceSockets[nsId].on("nsChange", (data) => {
+      console.log("namespace changed!");
+      console.log(data);
+    });
+    listeners.nsChange[nsId] = true;
+  } else {
+    // nothing to do listener has been added
+  }
+};
+
 socket.on("connect", () => {
-  console.log("Connected!");
+  // console.log("Connected!");
   socket.emit("clientConnect");
 });
 
-socket.on("welcome", (data) => {
+socket.on(" ", (data) => {
   console.log(data);
 });
 
 // listen for nsList event from the server
 socket.on("nsList", (nsData) => {
-  const lastNs = localStorage.getItem("lastNs") || "/wiki";
   const nsDiv = document.querySelector(".namespaces");
   nsDiv.innerHTML = "";
   nsData.forEach((ns) => {
     nsDiv.innerHTML += `<div class="namespace" ns=${ns.endpoint}><img src=${ns.image}></div>`;
 
-    // join this namespace with io()
-    const thisNsSocket = io(`http://localhost:9000${ns.endpoint}`); // /wiki, /mozilla, /linux
-    thisNsSocket.on("nsChange", (data) => {
-      console.log("namespace changed!");
-      console.log(data);
-    });
+    // initialize thisNameSpace as its index in nameSpaceSockets
+    // if the connection is new, this will be null
+    // if the connection has already been established, it will reconnect and remain in its spot
+    // let thisNs = nameSpaceSockets[ns.id];
+
+    if (!nameSpaceSockets[ns.id]) {
+      // there is no socket at this namespace id, so make new connection
+      // join this namespace with io()
+      nameSpaceSockets[ns.id] = io(`http://localhost:9000${ns.endpoint}`); // /wiki, /mozilla, /linux
+    }
+    addListeners(ns.id);
   });
 
   Array.from(document.getElementsByClassName("namespace")).forEach((element) => {
@@ -39,6 +61,7 @@ socket.on("nsList", (nsData) => {
     });
   });
 
+  const lastNs = localStorage.getItem("lastNs") || "/wiki";
   // if lastNs is set, grab the element and set it
   const lastNsElement = document.querySelector(`.namespace[ns="${lastNs}"]`);
   if (lastNsElement) joinNs(lastNsElement, nsData);
